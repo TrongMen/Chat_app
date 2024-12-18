de566bdfeb823e34c09a025746efc1ba125b6fb2
TrongMen <htm2810002@gmail.com>
2024-12-19 00:06:23 +0700

    Update resetpassword again

--- Front_end/src/pages/auth/ResetPassword.js
+++ Front_end/src/pages/auth/ResetPassword.js
@@ -1,6 +1,7 @@
-import { Stack, Typography } from "@mui/material";
+import { Link, Stack, Typography } from "@mui/material";
+import { CaretLeft } from "phosphor-react";
 import React from "react";
-
+import { Link as RouterLink } from "react-router-dom";
 const ResetPassword = () => {
   return (
     <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
@@ -8,6 +9,18 @@ const ResetPassword = () => {
       <Typography sx={{ color: "text.secondary", mb: 5 }}>
         Vui lòng nhập email và chúng tôi gửi link để bạn đổi lại mật khẩu
       </Typography>
+      {/* ResetPassword Form */}
+
+      <Link
+        component={RouterLink}
+        to={"/auth/login"}
+        color={"inherit"}
+        variant="subtitle2"
+        sx={{ mt: 3, mx: "auto", alignItems: "center" }}
+      >
+        <CaretLeft />
+        Quay lại trang đăng nhập
+      </Link>
     </Stack>
   );
 };
