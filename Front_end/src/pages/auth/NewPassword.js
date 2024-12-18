84bbf648bb820c550e20cfe2dd221c5ec6d7134b
TrongMen <htm2810002@gmail.com>
2024-12-19 00:26:08 +0700

    update NewPassword 1

--- Front_end/src/pages/auth/NewPassword.js
+++ Front_end/src/pages/auth/NewPassword.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Stack, Typography } from "@mui/material";
+import React from "react";
 
 const NewPassword = () => {
   return (
-    <div>NewPassword</div>
-  )
-}
+    <Stack
+      spacing={2}
+      sx={{
+        mb: 5,
+        position: "relative",
+        backgroundColor: "rgba(244, 240, 240, 0.8)",
+        borderRadius: "20px",
+        maxWidth: "100%",
+        boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <Typography variant="h3" p={1}>
+        Đổi lại mật khẩu
+      </Typography>
+      <Typography sx={{ color: "text.primary", mb: 5 }}>
+        Vui lòng nhập mật khẩu mới
+      </Typography>
+    </Stack>
+    // NewPS Form
+  );
+};
 
-export default NewPassword
+export default NewPassword;
