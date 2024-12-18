cb088b83627d0133d41845941567a458bf6014e8
TrongMen <htm2810002@gmail.com>
2024-12-19 00:00:17 +0700

    Update register FE

--- Front_end/src/pages/auth/Register.js
+++ Front_end/src/pages/auth/Register.js
@@ -1,53 +1,53 @@
-// import { Link, Stack, Typography } from "@mui/material";
-// import React from "react";
-// import { Link as RouterLink } from "react-router-dom";
-// import RegisterForm from "../../sections/auth/RegisterForm";
-// const Register = () => {
-//   return (
-//     <Stack
-//       spacing={2}
-//       sx={{
-//         mb: 5,
-//         position: "relative",
-//         backgroundColor: "rgba(244, 240, 240, 0.8)",
-//         borderRadius: "20px",
-//         maxWidth: "100%",
-//         boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
-//       }}
-//     >
-//       <Typography variant="h4" p={1}>Đăng ký</Typography>
+import { Link, Stack, Typography } from "@mui/material";
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import RegisterForm from "../../sections/auth/RegisterForm";
+const Register = () => {
+  return (
+    <Stack
+      spacing={2}
+      sx={{
+        mb: 5,
+        position: "relative",
+        backgroundColor: "rgba(244, 240, 240, 0.8)",
+        borderRadius: "20px",
+        maxWidth: "100%",
+        boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <Typography variant="h4" p={1}>Đăng ký</Typography>
 
       
-//       {/* Form đăng ký */}
-//       <RegisterForm />
-//       <Typography
-//         component={"div"}
-//         sx={{
-//           color: "text.secondary",
-//           mt: 3,
-//           typography: "caption",
-//           textAlign: "center",
+      {/* Form đăng ký */}
+      <RegisterForm />
+      <Typography
+        component={"div"}
+        sx={{
+          color: "text.secondary",
+          mt: 3,
+          typography: "caption",
+          textAlign: "center",
           
-//         }}
+        }}
         
-//       >
-//         {"Bằng cách đăng ký, tôi đồng ý"}
-//         <Link underline="always" color={"text.primary"} p={0.5}>
-//           Điều khoản dịch vụ 
-//         </Link>
-//         {"và"}
-//         <Link underline="always" color={"text.primary"} p={0.5}>
-//           Chính sách bảo mật 
-//         </Link>
-//       </Typography>
-//       <Stack direction={"row"} spacing={1} p={1}>
-//         <Typography>Bạn đã có tài khoản?</Typography>
-//         <Link component={RouterLink} to={"/auth/login"} variant="subtitle2">
-//           Đăng nhập
-//         </Link>
-//       </Stack>
-//     </Stack>
-//   );
-// };
+      >
+        {"Bằng cách đăng ký, tôi đồng ý"}
+        <Link underline="always" color={"text.primary"} p={0.5}>
+          Điều khoản dịch vụ 
+        </Link>
+        {"và"}
+        <Link underline="always" color={"text.primary"} p={0.5}>
+          Chính sách bảo mật 
+        </Link>
+      </Typography>
+      <Stack direction={"row"} spacing={1} p={1}>
+        <Typography>Bạn đã có tài khoản?</Typography>
+        <Link component={RouterLink} to={"/auth/login"} variant="subtitle2">
+          Đăng nhập
+        </Link>
+      </Stack>
+    </Stack>
+  );
+};
 
-// export default Register;
+export default Register;
--- Front_end/src/pages/auth/ResetPassword.js
+++ Front_end/src/pages/auth/ResetPassword.js
@@ -1,15 +1,15 @@
-import { Stack, Typography } from "@mui/material";
-import React from "react";
+// import { Stack, Typography } from "@mui/material";
+// import React from "react";
 
-const ResetPassword = () => {
-  return (
-    <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
-      <Typography variant="h3">Quên mật khẩu?</Typography>
-      <Typography sx={{ color: "text.secondary", mb: 5 }}>
-        Vui lòng nhập email và chúng tôi gửi link để bạn đổi lại mật khẩu
-      </Typography>
-    </Stack>
-  );
-};
+// const ResetPassword = () => {
+//   return (
+//     <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
+//       <Typography variant="h3">Quên mật khẩu?</Typography>
+//       <Typography sx={{ color: "text.secondary", mb: 5 }}>
+//         Vui lòng nhập email và chúng tôi gửi link để bạn đổi lại mật khẩu
+//       </Typography>
+//     </Stack>
+//   );
+// };
 
-export default ResetPassword;
+// export default ResetPassword;
