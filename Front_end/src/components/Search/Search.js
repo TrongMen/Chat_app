10e7c5ebf15c104137cd51812395b25510eece19
TrongMen <htm2810002@gmail.com>
2024-12-20 00:10:26 +0700

    update Search

--- Front_end/src/components/Search/Search.js
+++ Front_end/src/components/Search/Search.js
@@ -1,11 +1,10 @@
+import { alpha, styled } from "@mui/material/styles";
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: 20,
+  backgroundColor: alpha(theme.palette.background.default, 1), // vừa sửa default
+  marginLeft: 0,
+  width: "100%",
+}));
 
-// import { alpha, styled } from "@mui/material/styles";
-// const Search = styled("div")(({ theme }) => ({
-//     position: "relative",
-//     borderRadius: 20,
-//     backgroundColor: alpha(theme.palette.background.default, 1), // vừa sửa default
-//     marginLeft: 0,
-//     width: "100%",
-//   }));
-
-//   export default Search;
+export default Search;
