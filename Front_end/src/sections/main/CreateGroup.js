e8656324f06119fc905cf902eec0c6725cc0f7be
TrongMen <htm2810002@gmail.com>
2024-12-20 01:18:50 +0700

    update creatGroup

--- Front_end/src/sections/main/CreateGroup.js
+++ Front_end/src/sections/main/CreateGroup.js
@@ -1,8 +1,61 @@
-import { Dialog, DialogContent, DialogTitle, Slide } from "@mui/material";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Slide,
+  Stack,
+} from "@mui/material";
 import React from "react";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import FormProvider, { RHFTextField } from "../../components/hook-form";
+
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
+
+const CreateGroupForm = ({}) => {
+  const NewGroupSchema = Yup.object().shape({
+    title: Yup.string().required("Nhập tên nhóm"),
+    members: Yup.array().min(2, "Phải có ít nhất 2 thành viên"),
+  });
+
+  const defaultValues = {
+    title: "",
+    members: [],
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(NewGroupSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    setError,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful, isValid },
+  } = methods;
+
+  const onSubmit = async (data) => {
+    try {
+      console.log("DATA ", data);
+    } catch (error) {
+      console.log("error obSubmit", error);
+      reset();
+      setError("afterSubmit", { ...error, message: error.message });
+    }
+  };
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        <RHFTextField name={"Tên nhóm"} label="Nhập tên nhóm" />
+      </Stack>
+    </FormProvider>
+  );
+};
 const CreateGroup = ({ open, handleClose }) => {
   return (
     <Dialog
@@ -11,13 +64,15 @@ const CreateGroup = ({ open, handleClose }) => {
       open={open}
       TransitionComponent={Transition}
       keepMounted
-      sx={{p:4}}
+      sx={{ p: 4 }}
     >
       {/* Title */}
       <DialogTitle>Tạo nhóm mới</DialogTitle>
       {/* Content */}
       <DialogContent>
-        
+        {/* Form */}
+
+        <CreateGroupForm />
       </DialogContent>
     </Dialog>
   );
