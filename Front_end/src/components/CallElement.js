ba4462d34c1c0c7c96d342bd184aeb638fa61040
TrongMen <htm2810002@gmail.com>
2024-12-20 10:52:37 +0700

    update CallElement

--- Front_end/src/components/CallElement.js
+++ Front_end/src/components/CallElement.js
@@ -1,29 +1,44 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { faker } from "@faker-js/faker";
+import { Avatar, Box } from "@mui/material";
+import React from "react";
+import StyledBadge from "./StyledBadge";
 
-const CallLogElement = () => {
-    return (
-        <Box
-        sx={{
-          width: "100%",
-  
-          borderRadius: 1,
-          backgroundColor:(theme) =>
-            theme.palette.mode === "light"
-              ? "#fff"
-              : theme.palette.background.default, // vừa sửa default
-        }}
-        p={1.5}
-      >
-
-        
-      </Box>
-    )
-  }
-const CallElement = () => {
+const CallLogElement = ({online}) => {
   return (
-    <div>CallElement</div>
-  )
-}
+    <Box
+      sx={{
+        width: "100%",
 
-export  {CallLogElement,CallElement };
+        borderRadius: 1,
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? "#fff"
+            : theme.palette.background.default, // vừa sửa default
+      }}
+      p={1.5}
+    >
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        {online ? (
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            variant="dot"
+          >
+            <Avatar src={faker.image.avatar()} alt={faker.name.fullName()} />
+          </StyledBadge>
+        ) : (
+          <Avatar src={faker.image.avatar()} alt={faker.name.fullName()} />
+        )}
+      </Stack>
+    </Box>
+  );
+};
+const CallElement = () => {
+  return <div>CallElement</div>;
+};
+
+export { CallLogElement, CallElement };
