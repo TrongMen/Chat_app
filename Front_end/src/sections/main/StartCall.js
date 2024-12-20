70d10db8023af8c791d2ae61ac85fc867e1acd55
TrongMen <htm2810002@gmail.com>
2024-12-20 11:30:06 +0700

    update StartCall

--- Front_end/src/sections/main/StartCall.js
+++ Front_end/src/sections/main/StartCall.js
@@ -1,5 +1,7 @@
-import { Dialog, DialogContent, DialogTitle, Slide } from "@mui/material";
+import { Dialog, DialogContent, DialogTitle, Slide, Stack } from "@mui/material";
 import React from "react";
+import { Search, SearchInconWrapper, StyledInputBase } from "../../components/Search";
+import { MagnifyingGlass } from "phosphor-react";
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
@@ -12,13 +14,25 @@ const StartCall = ({ open, handleClose }) => {
       TransitionComponent={Transition}
       keepMounted
       sx={{ p: 4 }}
+      onClose={handleClose}
     >
-      <DialogTitle sx={{ mb: 3 }}>Bắt đầu cuộc gọi</DialogTitle>
+      <DialogTitle sx={{ mb: 3 }}>Bắt đầu cuộc gọi mới</DialogTitle>
       {/* Content */}
       <DialogContent>
         {/* Form */}
-
-        <CreateGroupForm handleClose={handleClose} />
+        <Stack sx={{ width: "100%" }} className="ThanhTimKiem">
+          <Search>
+            <SearchInconWrapper>
+              <MagnifyingGlass color="#709CE6" />
+            </SearchInconWrapper>
+            <StyledInputBase
+              placeholder="Nhập tên bạn bè ..."
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Stack>
+        {/* Call List */}
+        {/* <CreateGroupForm handleClose={handleClose} /> */}
       </DialogContent>
     </Dialog>
   );
