692ae8bcfefa7d6a1e5d6295a6267e0a7e60b094
TrongMen <htm2810002@gmail.com>
2024-12-24 23:49:30 +0700

    update Auth

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -30,32 +30,28 @@ exports.register = catchAsync(async (req, res, next) => {
     "password"
   );
 
-  // check if a verified user with given email exists
-
   const existing_user = await User.findOne({ email: email });
 
   if (existing_user && existing_user.verified) {
-    // user with this email already exists, Please login
+
     return res.status(400).json({
       status: "error",
       message: "Email already in use, Please login.",
     });
   } else if (existing_user) {
-    // if not verified than update prev one
+
 
     await User.findOneAndUpdate({ email: email }, filteredBody, {
       new: true,
       validateModifiedOnly: true,
     });
 
-    // generate an otp and send to email
     req.userId = existing_user._id;
     next();
   } else {
-    // if user is not created before than create a new one
+
     const new_user = await User.create(filteredBody);
 
-    // generate an otp and send to email
     req.userId = new_user._id;
     next();
   }
@@ -152,7 +148,7 @@ exports.login = catchAsync(async (req, res, next) => {
   if (!email || !password) {
     res.status(400).json({
       status: "error",
-      message: "Both email and password are required",
+      message: "Cả email và mật khẩu là bắt buộc",
     });
     return;
   }
