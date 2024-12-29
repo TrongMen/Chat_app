a484c430cee3e5a1eaafaf8da18936523e07c165
TrongMen <htm2810002@gmail.com>
2024-12-29 16:44:35 +0700

    update field Verify FE

--- Front_end/src/components/hook-form/RHFCode.js
+++ Front_end/src/components/hook-form/RHFCode.js
@@ -5,6 +5,21 @@ import { TextField } from "@mui/material";
 const RHFCode = ({ keyName = "", inputs = [], ...other }) => {
   const codesRef = useRef(null);
   const { control } = useFormContext();
+  const handleChangeWithNextField = (event, handleChange) => {
+    const { maxLength, value, name } = event.target;
+    const fieldIndex = name.replace(keyName, "");
+    const fieldIntIndex = Number(fieldIndex);
+    const nextField = document.querySelector(
+      `input[name=${keyName}${fieldIntIndex}]`
+    );
+    if (value.length > maxLength) {
+      event.target.value = value[0];
+    }
+    if (value.length >= maxLength && fieldIntIndex < 6 && nextField !== null) {
+      nextField.focus();
+    }
+    handleChange();
+  };
   return (
     <Stack
       direction={"row"}
@@ -23,8 +38,20 @@ const RHFCode = ({ keyName = "", inputs = [], ...other }) => {
               error={!!error}
               autoFocus={index === 0}
               placeholder={"-"}
-              otherChange={(event) => {}}
-              
+              onChange={(event) => {}}
+              onFocus={(event) => event.currentTarget.select()}
+              InputProps={{
+                sx: {
+                  width: { xs: 36, sm: 56 },
+                  height: { xs: 36, sm: 56 },
+                  "& input": { p: 0, textAlign: "center" },
+                },
+              }}
+              inputProps={{
+                maxLength: 1,
+                type: "number",
+              }}
+              {...other}
             />
           )}
         ></Controller>
