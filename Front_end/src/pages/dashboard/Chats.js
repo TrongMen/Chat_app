2d78816691c71f09c2a680eea960593bd3cd7216
TrongMen <htm2810002@gmail.com>
2025-01-01 02:18:51 +0700

    File chat dang gap loi

--- Front_end/src/pages/dashboard/Chats.js
+++ Front_end/src/pages/dashboard/Chats.js
@@ -6,7 +6,12 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
+import {
+  ArchiveBox,
+  CircleDashed,
+  MagnifyingGlass,
+  Users,
+} from "phosphor-react";
 import React from "react";
 import { useTheme } from "@mui/material/styles";
 import { ChatList } from "../../data";
@@ -17,76 +22,95 @@ import {
   StyledInputBase,
 } from "../../components/Search";
 import ChatElement from "../../components/ChatElement";
+import Friends from "../../sections/main/Friends";
 
 const Chats = () => {
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+  const handleOpenDialog = () => {
+    setOpenDialog(false);
+  };
   const theme = useTheme();
   return (
-    <Box
-      sx={{
-        position: "relative",
-        width: 320,
-        backgroundColor:
-          theme.palette.mode === "light"
-            ? "#F8FAFF"
-            : theme.palette.background.paper, //vừa sửa paper
-        boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
-      }}
-    >
-      <Stack p={3} spacing={2} sx={{ height: "100vh" }}>
-        <Stack
-          direction={"row"}
-          alignItems={"center"}
-          justifyContent={"space-between"}
-        >
-          <Typography variant="h5">Chats</Typography>
-          <IconButton>
-            <CircleDashed size={32} />
-          </IconButton>
-        </Stack>
-        <Stack sx={{ width: "100%" }}>
-          <Search>
-            <SearchInconWrapper>
-              <MagnifyingGlass color="#709CE6" />
-            </SearchInconWrapper>
-            <StyledInputBase
-              placeholder="Tìm ..."
-              inputProps={{ "aria-label": "search" }}
-            />
-          </Search>
-        </Stack>
-        <Stack spacing={2}>
-          <Stack direction={"row"} alignItems={"center"} spacing={1.5}>
-            <ArchiveBox size={24}></ArchiveBox>
-            <Button> Archive</Button>
+    <>
+      <Box
+        sx={{
+          position: "relative",
+          width: 320,
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#F8FAFF"
+              : theme.palette.background.paper, //vừa sửa paper
+          boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
+        }}
+      >
+        <Stack p={3} spacing={2} sx={{ height: "100vh" }}>
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
+            <Typography variant="h5">Chats</Typography>
+            <Stack direction={"row"} alignItems={"center"} spacing={1.5}>
+              <IconButton onClick={() => {}}>
+                <Users size={32} />
+              </IconButton>
+              <IconButton>
+                <CircleDashed size={32} />
+              </IconButton>
+            </Stack>
           </Stack>
-          <Divider />
-        </Stack>
-        <Stack
-          spacing={2}
-          direction={"column"}
-          sx={{ flexGrow: 1, overflow: "scroll", height: "100%" }}
-        >
-          <SimpleBarStyle timeout={500} clickOnTrack={false}>
-            <Stack spacing={2.4}>
-              <Typography variant="subtitle2" sx={{ color: "#676767" }}>
-                Ghim tin nhắn
-              </Typography>
-              {ChatList.filter((el) => el.pinned).map((el) => {
-                return <ChatElement key={el.id} {...el} />;
-              })}
+          <Stack sx={{ width: "100%" }}>
+            <Search>
+              <SearchInconWrapper>
+                <MagnifyingGlass color="#709CE6" />
+              </SearchInconWrapper>
+              <StyledInputBase
+                placeholder="Tìm ..."
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+          </Stack>
+          <Stack spacing={2}>
+            <Stack direction={"row"} alignItems={"center"} spacing={1.5}>
+              <ArchiveBox size={24}></ArchiveBox>
+              <Button> Archive</Button>
             </Stack>
-            <Stack spacing={2.4}>
-              <Typography variant="subtitle2" sx={{ color: "#676767" }}>
-                Tin nhắn
-              </Typography>
-              {ChatList.filter((el) => !el.pinned).map((el) => {
-                return <ChatElement key={el.id} {...el} />;
-              })}
-            </Stack>
-          </SimpleBarStyle>
+            <Divider />
+          </Stack>
+          <Stack
+            spacing={2}
+            direction={"column"}
+            sx={{ flexGrow: 1, overflow: "scroll", height: "100%" }}
+          >
+            <SimpleBarStyle timeout={500} clickOnTrack={false}>
+              <Stack spacing={2.4}>
+                <Typography variant="subtitle2" sx={{ color: "#676767" }}>
+                  Ghim tin nhắn
+                </Typography>
+                {ChatList.filter((el) => el.pinned).map((el) => {
+                  return <ChatElement key={el.id} {...el} />;
+                })}
+              </Stack>
+              <Stack spacing={2.4}>
+                <Typography variant="subtitle2" sx={{ color: "#676767" }}>
+                  Tin nhắn
+                </Typography>
+                {ChatList.filter((el) => !el.pinned).map((el) => {
+                  return <ChatElement key={el.id} {...el} />;
+                })}
+              </Stack>
+            </SimpleBarStyle>
+          </Stack>
         </Stack>
-      </Stack>
-    </Box>
+      </Box>
+      {openDialog && (
+        <Friends open={openDialog} handleClose={handleCloseDialog} />
+      )}
+    </>
   );
 };
 
